d3ef9528145a91a891ae9b15f1f562e8f215f571
Babosa <babosa@easydarwin.org>
2019-01-07 14:33:48 +0800

    EasyPlayer-RTSP v2.3.19.0106

--- Src/C++/EasyPlayerWebActiveX/EasyPlayer-RTSPWebActiveXidl.h
+++ Src/C++/EasyPlayerWebActiveX/EasyPlayer-RTSPWebActiveXidl.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Thu Sep 27 17:07:42 2018
+/* at Mon Jan 07 14:28:10 2019
  */
 /* Compiler settings for EasyPlayerWebActiveX.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
