af76a55b66787d39b79bffa26e94c9ce5462ae34
Babosa <babosa@easydarwin.org>
2019-07-02 18:14:18 +0800

    update KEY for EasyPlayer-RTSP.exe 90days

--- Src/C#/EasyPlayerNetSDK/PlayerSdk.cs
+++ Src/C#/EasyPlayerNetSDK/PlayerSdk.cs
@@ -116,7 +116,7 @@ namespace EasyPlayerNetSDK
         /// </summary>
         /// <returns>System.Int32.</returns>
         [DllImport(@"Lib\libEasyPlayer-RTSP.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "?EasyPlayer_Init@@YAHPAD@Z")]
-        public static extern int EasyPlayer_Init(string key= "6D75724D7A4969576B5A7541725370636F395652792B784659584E3555477868655756794C564A55553141755A58686C567778576F50394C2F69426C59584E35");
+        public static extern int EasyPlayer_Init(string key= "6D75724D7A4969576B5A734154526C646F7561797065784659584E3555477868655756794C564A55553141755A58686C567778576F502B6C2F69426C59584E35");
 
         /// <summary>
         /// 资源释放.
